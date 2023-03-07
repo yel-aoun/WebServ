e3f428a7651d8bd2349eaadd918ba9d9ac8865e1
yeeticus2000 <ouazahrouz@gmail.com>
2023-03-07 11:26:25 +0100

    ::

--- parsing/location.hpp
+++ parsing/location.hpp
@@ -11,16 +11,24 @@ class location
 {
     private:
     std::string locations;
-    std::string allow_methods;
+    std::list<std::string> allow_methods;
     std::string redirect;
     std::string root;
     std::string index;
     std::string cgi_pass;
     std::string upload_pass;
+    std::string auto_index;
     public:
     location(){}
     ~location(){}
     location(const std::list<std::string> &config);
     void FillLocation(std::string prompt);
+    void FillAllow_methods(std::string prompt);
+    void FillIndex(std::string prompt);
+    void FillRedirect(std::string prompt);
+    void FillRoot(std::string prompt);
+    void FillCgi_pass(std::string prompt);
+    void FillUpload_pass(std::string prompt);
+    void FillAuto_index(std::string prompt);
 };
 # endif
