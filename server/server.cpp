246734710ba5b9d0225df92f0691fd93a191d5e9
hamzazaouya <hamzazaouya18@gmail.com>
2023-03-09 18:43:54 +0100

    accept new connection

--- server/server.cpp
+++ server/server.cpp
@@ -1,11 +1,97 @@
 #include "server.hpp"
 
+Server::Server(PORT port):_port(port)
+{
+    Socket socket(this->_port);
+    this->_server_socket = socket.get_socket();
+}
 
-Server::Server(std::string port): _host(0), _port(port) {}
-Server::Server(std::string host, std::string port): _host(host), _port(port) {}
+// this function is init sockfds for a serve and clients.
+void  Server::init_sockfds()
+{
+    FD_ZERO(&this->_reads);
+    FD_SET(this->_server_socket, &this->_reads);
+    this->_max_socket = this->_server_socket;
+    std::list<Client>::iterator client_iter;
 
+    for(client_iter = this->_clients.begin(); client_iter != this->_clients.end(); client_iter++)
+    {
+        FD_SET(client_iter->get_sockfd(), &this->_reads);
+        if (client_iter->get_sockfd() > this->_max_socket)
+            this->_max_socket = client_iter->get_sockfd();
+    }
+}
 
-fd_set  wait_on_clients(SOCKET server);
-Client  get_client(SOCKET s);
-void    drop_client(Client client);
-void    serve_resource(Client client, const char *path);
+// this function tell some connection comes,
+// and we know that the connection happened when select detects changes in the server socket.
+// and select have 1 second to check if no connection happened we have to move one to serve clients that already connected
+void    Server::wait_on_clients()
+{
+    struct timeval restrict;
+
+    this->init_sockfds();
+    restrict.tv_sec = 5;
+    restrict.tv_usec = 0;
+    std::cout << "max socket = " << this->_max_socket  << std::endl;
+    if (select(this->_max_socket + 1, &this->_reads, NULL, NULL, &restrict) < 0)
+    {
+        std::cerr << "select() failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+const char *get_client_address(Client *ci)
+{
+    static char address_buffer[100];
+    getnameinfo((struct sockaddr*)&ci->_address,
+    ci->_address_length,
+    address_buffer, sizeof(address_buffer), 0, 0,
+    NI_NUMERICHOST);
+    return address_buffer;
+}
+
+void    Server::accept_new_client()
+{
+    Client client;
+
+    client.set_sockfd(accept(this->_server_socket, \
+        reinterpret_cast<struct sockaddr *>(&client._address), \
+        &(client._address_length)));
+    if (!ISVALIDSOCKET(client.get_sockfd()))
+    {
+        std::cerr << "accept() failed." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    this->_clients.push_back(client);
+    std::cout << "New connection from : " << get_client_address(&client) << std::endl;
+}
+
+void    Server::run_serve()
+{
+    int i = 0;
+    while(1)
+    {
+        this->wait_on_clients();
+        if (FD_ISSET(this->_server_socket, &this->_reads))
+            this->accept_new_client();
+    }
+}
+
+void    Server::drop_client(Client &client)
+{
+    CLOSESOCKET(client.get_sockfd());
+    std::list<Client>::iterator iter;
+
+    for(iter = this->_clients.begin(); iter != this->_clients.end(); iter++)
+    {
+        if(client.get_sockfd() == (*iter).get_sockfd())
+            iter = this->_clients.erase(iter);
+        return ;
+    }
+    std::cerr << "Drop Client not found !" << std::endl;
+}
+
+Server::~Server()
+{
+    // close server socket;
+}
--- server/socket.cpp
+++ server/socket.cpp
@@ -1,6 +1,6 @@
 #include "socket.hpp"
 
-Socket::Socket(char *port): _port(port)
+Socket::Socket(int  port): _port(port)
 {
     this->creat_socket();
 }
@@ -13,9 +13,10 @@ Socket::Socket(const Socket &rhs)
 Socket& Socket::operator = (const Socket& rhs)
 {
     this->_sockfd = rhs._sockfd;
+    return (*this);
 }
 
-void    Socket::init_socket(void)
+void    Socket::init_socket()
 {
     this->_sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (!ISVALIDSOCKET(this->_sockfd))
@@ -33,14 +34,14 @@ void    Socket::bind_socket()
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(this->_port);
-    if(bind(this->_sockfd, static_cast<const sockaddr *>(&addr), sizeof(addr)))
+    if(bind(this->_sockfd, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)))
     {
         std::cerr << "bind() failed." << GETSOCKETERRNO() << std::endl;
         std::exit(EXIT_FAILURE);
     }
 }
 
-void    Socket::listen_socket(void)
+void    Socket::listen_socket()
 {
     if(listen(this->_sockfd, 10))
     {
@@ -49,16 +50,16 @@ void    Socket::listen_socket(void)
     }
 }
 
-void  Socket::creat_socket(void)
+void  Socket::creat_socket()
 {
     this->init_socket();
     this->bind_socket();
     this->listen_socket();
 }
 
-Socket  Socket::get_socket(void) const
+SOCKET  Socket::get_socket(void) const
 {
-    return (this->_socket)
+    return (this->_sockfd);
 }
 
 Socket::~Socket(){}
