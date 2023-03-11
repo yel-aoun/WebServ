e47b19dbbb4a89fcd57e3cbb1776e845e2e5b383
yeeticus2000 <ouazahrouz@gmail.com>
2023-03-11 16:22:50 +0100

    Save

--- srcs/parsing/parce_server.cpp
+++ srcs/parsing/parce_server.cpp
@@ -109,10 +109,7 @@ parce_server::parce_server(const std::list<std::string> &conf, int n_serv)
     for(; it != conf.end(); it++)
     {
         if ((*it).empty())
-        {
             for (; (*it).empty() != 0; ++it)
-                it++;
-        }
         if ((it->find("location") != -1 && it->rfind("{") != -1) && ind)
             break;
         if (it->find("location") != -1 && it->rfind("{") != -1)
